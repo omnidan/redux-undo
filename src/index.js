a0c61f1a77971ab0ac2f0d3c0bee48e9804805c9
Daniel Bugl <daniel.bugl@touchlay.com>
2015-10-10 02:57:15 +0200

    use JS standard code style

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 // debug output
-let __DEBUG__;
-function debug(...args) {
+let __DEBUG__
+function debug (...args) {
   if (__DEBUG__) {
     if (!console.group) {
-      args.unshift('%credux-undo', 'font-style: italic');
+      args.unshift('%credux-undo', 'font-style: italic')
     }
-    console.log(...args);
+    console.log(...args)
   }
 }
-function debugStart(action, state) {
+function debugStart (action, state) {
   if (__DEBUG__) {
-    const args = ['action', action.type];
+    const args = ['action', action.type]
     if (console.group) {
-      args.unshift('%credux-undo', 'font-style: italic');
-      console.groupCollapsed(...args);
-      console.log('received', {state, action});
+      args.unshift('%credux-undo', 'font-style: italic')
+      console.groupCollapsed(...args)
+      console.log('received', {state, action})
     } else {
-      debug(...args);
+      debug(...args)
     }
   }
 }
-function debugEnd() {
+function debugEnd () {
   if (__DEBUG__) {
-    return console.groupEnd && console.groupEnd();
+    return console.groupEnd && console.groupEnd()
   }
 }
 // /debug output
@@ -30,137 +30,137 @@ function debugEnd() {
 // action types
 export const ActionTypes = {
   UNDO: '@@redux-undo/UNDO',
-  REDO: '@@redux-undo/REDO',
-};
+  REDO: '@@redux-undo/REDO'
+}
 // /action types
 
 // action creators to change the state
 export const ActionCreators = {
-  undo() {
-    return { type: ActionTypes.UNDO };
+  undo () {
+    return { type: ActionTypes.UNDO }
   },
-  redo() {
-    return { type: ActionTypes.REDO };
-  },
-};
+  redo () {
+    return { type: ActionTypes.REDO }
+  }
+}
 // /action creators
 
 // length: get length of history
-function length(history) {
-  const { past, future } = history;
-  return past.length + 1 + future.length;
+function length (history) {
+  const { past, future } = history
+  return past.length + 1 + future.length
 }
 // /length
 
 // insert: insert `state` into history, which means adding the current state
 //         into `past`, setting the new `state` as `present` and erasing
 //         the `future`.
-function insert(history, state, limit) {
-  debug('insert', {state, history, free: limit - length(history)});
+function insert (history, state, limit) {
+  debug('insert', {state, history, free: limit - length(history)})
 
-  const { past, present } = history;
-  const historyOverflow = limit && length(history) >= limit;
+  const { past, present } = history
+  const historyOverflow = limit && length(history) >= limit
 
   if (present === undefined) {
     // init history
     return {
       past: [],
       present: state,
-      future: [],
-    };
+      future: []
+    }
   }
 
   return {
     past: [
       ...past.slice(historyOverflow ? 1 : 0),
-      present,
+      present
     ],
     present: state,
-    future: [],
-  };
+    future: []
+  }
 }
 // /insert
 
 // undo: go back to the previous point in history
-function undo(history) {
-  debug('undo', {history});
+function undo (history) {
+  debug('undo', {history})
 
-  const { past, present, future } = history;
+  const { past, present, future } = history
 
-  if (past.length <= 0) return history;
+  if (past.length <= 0) return history
 
   return {
     past: past.slice(0, past.length - 1), // remove last element from past
     present: past[past.length - 1], // set element as new present
     future: [
       present, // old present state is in the future now
-      ...future,
-    ],
-  };
+      ...future
+    ]
+  }
 }
 // /undo
 
 // redo: go to the next point in history
-function redo(history) {
-  debug('redo', {history});
+function redo (history) {
+  debug('redo', {history})
 
-  const { past, present, future } = history;
+  const { past, present, future } = history
 
-  if (future.length <= 0) return history;
+  if (future.length <= 0) return history
 
   return {
     future: future.slice(1, future.length), // remove element from future
     present: future[0], // set element as new present
     past: [
       ...past,
-      present, // old present state is in the past now
-    ],
-  };
+      present // old present state is in the past now
+    ]
+  }
 }
 // /redo
 
 // wrapState: for backwards compatibility to 0.4
-function wrapState(state) {
+function wrapState (state) {
   return {
     ...state,
-    history: state,
-  };
+    history: state
+  }
 }
 // /wrapState
 
 // updateState
-function updateState(state, history) {
+function updateState (state, history) {
   return wrapState({
     ...state,
-    ...history,
-  });
+    ...history
+  })
 }
 // /updateState
 
 // createHistory
-function createHistory(state) {
+function createHistory (state) {
   return {
     past: [],
     present: state,
-    future: [],
-  };
+    future: []
+  }
 }
 // /createHistory
 
 // parseActions
-export function parseActions(rawActions, defaultValue = []) {
+export function parseActions (rawActions, defaultValue = []) {
   if (Array.isArray(rawActions)) {
-    return rawActions;
+    return rawActions
   } else if (typeof rawActions === 'string') {
-    return [rawActions];
+    return [rawActions]
   }
-  return defaultValue;
+  return defaultValue
 }
 // /parseActions
 
 // redux-undo higher order reducer
-export default function undoable(reducer, rawConfig = {}) {
-  __DEBUG__ = rawConfig.debug;
+export default function undoable (reducer, rawConfig = {}) {
+  __DEBUG__ = rawConfig.debug
 
   const config = {
     initialState: rawConfig.initialState,
@@ -168,90 +168,90 @@ export default function undoable(reducer, rawConfig = {}) {
     limit: rawConfig.limit,
     filter: rawConfig.filter || () => true,
     undoType: rawConfig.undoType || ActionTypes.UNDO,
-    redoType: rawConfig.redoType || ActionTypes.REDO,
-  };
-  config.history = rawConfig.initialHistory || createHistory(config.initialState);
+    redoType: rawConfig.redoType || ActionTypes.REDO
+  }
+  config.history = rawConfig.initialHistory || createHistory(config.initialState)
 
   if (config.initTypes.length === 0) {
-    console.warn('redux-undo: supply at least one action type in initTypes to ensure initial state');
+    console.warn('redux-undo: supply at least one action type in initTypes to ensure initial state')
   }
 
   return (state, action) => {
-    debugStart(action, state);
-    let res;
+    debugStart(action, state)
+    let res
     switch (action.type) {
-    case config.undoType:
-      res = undo(state);
-      debug('after undo', res);
-      debugEnd();
-      return res ? updateState(state, res) : state;
+      case config.undoType:
+        res = undo(state)
+        debug('after undo', res)
+        debugEnd()
+        return res ? updateState(state, res) : state
 
-    case config.redoType:
-      res = redo(state);
-      debug('after redo', res);
-      debugEnd();
-      return res ? updateState(state, res) : state;
+      case config.redoType:
+        res = redo(state)
+        debug('after redo', res)
+        debugEnd()
+        return res ? updateState(state, res) : state
 
-    default:
-      res = reducer(state && state.present, action);
+      default:
+        res = reducer(state && state.present, action)
 
-      if (config.initTypes.some((actionType) => actionType === action.type)) {
-        debug('reset history due to init action');
-        debugEnd();
-        return wrapState({
-          ...state,
-          ...createHistory(res),
-        });
-      }
-
-      if (config.filter && typeof config.filter === 'function') {
-        if (!config.filter(action, res, state && state.present)) {
-          debug('filter prevented action, not storing it');
-          debugEnd();
+        if (config.initTypes.some((actionType) => actionType === action.type)) {
+          debug('reset history due to init action')
+          debugEnd()
           return wrapState({
             ...state,
-            present: res,
-          });
+            ...createHistory(res)
+          })
         }
-      }
 
-      const history = (state && state.present !== undefined) ? state : config.history;
-      const updatedHistory = insert(history, res, config.limit);
-      debug('after insert', {history: updatedHistory, free: config.limit - length(updatedHistory)});
-      debugEnd();
+        if (config.filter && typeof config.filter === 'function') {
+          if (!config.filter(action, res, state && state.present)) {
+            debug('filter prevented action, not storing it')
+            debugEnd()
+            return wrapState({
+              ...state,
+              present: res
+            })
+          }
+        }
 
-      return wrapState({
-        ...state,
-        ...updatedHistory,
-      });
+        const history = (state && state.present !== undefined) ? state : config.history
+        const updatedHistory = insert(history, res, config.limit)
+        debug('after insert', {history: updatedHistory, free: config.limit - length(updatedHistory)})
+        debugEnd()
+
+        return wrapState({
+          ...state,
+          ...updatedHistory
+        })
     }
-  };
+  }
 }
 // /redux-undo
 
 // distinctState helper
-export function distinctState() {
-  return (action, currentState, previousState) => currentState !== previousState;
+export function distinctState () {
+  return (action, currentState, previousState) => currentState !== previousState
 }
 // /distinctState
 
 // includeAction helper
-export function includeAction(rawActions) {
-  const actions = parseActions(rawActions);
-  return (action) => actions.indexOf(action.type) >= 0;
+export function includeAction (rawActions) {
+  const actions = parseActions(rawActions)
+  return (action) => actions.indexOf(action.type) >= 0
 }
 // /includeAction
 
 // deprecated ifAction helper
-export function ifAction(rawActions) {
-  console.error('Deprecation Warning: Please change `ifAction` to `includeAction`');
-  return includeAction(rawActions);
+export function ifAction (rawActions) {
+  console.error('Deprecation Warning: Please change `ifAction` to `includeAction`')
+  return includeAction(rawActions)
 }
 // /ifAction
 
 // excludeAction helper
-export function excludeAction(rawActions = []) {
-  const actions = parseActions(rawActions);
-  return (action) => actions.indexOf(action.type) < 0;
+export function excludeAction (rawActions = []) {
+  const actions = parseActions(rawActions)
+  return (action) => actions.indexOf(action.type) < 0
 }
 // /excludeAction
