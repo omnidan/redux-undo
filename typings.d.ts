90e9204337ae71743b7c3c55e2e5d9db966b5b70
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-10-24 00:08:41 +0300

    Create typings.d.ts

---
+++ typings.d.ts
@@ -0,0 +1,68 @@
+import { Reducer, Action } from 'redux';
+
+module 'redux-undo' {
+  export interface StateWithHistory<State> {
+    past: State[];
+    present: State;
+    future: State[];
+  }
+
+  export type FilterFunction = (action: Action) => boolean;
+  export type combineFilters = (...filters: FilterFunction[]) => FilterFunction;
+
+  interface Options {
+    /* Set a limit for the history */
+    limit?: number;
+
+    /** If you don't want to include every action in the undo/redo history, you can add a filter function to undoable */
+    filter?: FilterFunction;
+
+    /** Define a custom action type for this undo action */
+    undoType?: string;
+    /** Define a custom action type for this redo action */
+    redoType?: string;
+
+    /** Define custom action type for this jump action */
+    jumpType?: string;
+
+    /** Define custom action type for this jumpToPast action */
+    jumpToPastType?: string;
+    /** Define custom action type for this jumpToFuture action */
+    jumpToFutureType?: string;
+
+    /** [beta only] Define custom action type for this clearHistory action */
+    clearHistoryType?: string;
+
+    /** History will be (re)set upon init action type */
+    initTypes?: string[];
+
+    /** Set to `true` to turn on debugging */
+    debug?: boolean; 
+  }
+
+  interface Undoable {
+    <State>(reducer: Reducer<State>, options?: Options): Reducer<StateWithHistory<State>>;
+  }
+
+
+  type includeAction = (actions: string | string[]) => FilterFunction;
+  type excludeAction = typeof includeAction;
+
+  const undoable: Undoable;
+
+  export default undoable;
+
+  /**
+   * If you don't want to include every action in the undo/redo history, you can add a filter function to undoable.
+   * redux-undo provides you with the includeAction and excludeAction helpers for basic filtering.
+   */
+  export const includeAction: includeAction;
+
+  /**
+   * If you don't want to include every action in the undo/redo history, you can add a filter function to undoable.
+   * redux-undo provides you with the includeAction and excludeAction helpers for basic filtering.
+   */
+  export const excludeAction: excludeAction;
+
+  export const combineFilters: combineFilters;
+}
