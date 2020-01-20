40c936f9b8753948a4768efd5532b33aa9a6c91e
Noah May <noahmouse2011@gmail.com>
2020-01-20 03:21:13 -0700

    Updated examples dependencies

---
+++ examples/todos-with-undo/README.md
@@ -0,0 +1,5 @@
+A basic example of how to use redux-undo.
+
+Simply run `npm install` then `npm start` to set started.
+
+Note: this build is not meant for production.
--- examples/todos-with-undo/components/Link.js
+++ examples/todos-with-undo/components/Link.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const Link = ({ active, children, onClick }) => {
   if (active) {
@@ -7,10 +8,10 @@ const Link = ({ active, children, onClick }) => {
 
   return (
     <a href="#"
-       onClick={e => {
-         e.preventDefault()
-         onClick()
-       }}
+      onClick={e => {
+        e.preventDefault()
+        onClick()
+      }}
     >
       {children}
     </a>
